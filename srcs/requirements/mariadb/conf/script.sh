6e74003bc08bc85bdcf4f6a677337960d9850feb
Taha Chtaibi <tchtaibi@e2r4p9.1337.ma>
2022-12-04 20:53:36 +0100

    theme

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -2,7 +2,6 @@ service mysql start
 mysql -u root -e  "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; flush privileges;"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; flush privileges;"
 mysql < /database.sql -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME}
-# mysqladmin -u root -pPASSWORD shutdown
 mysql -u root -e "flush privileges;"
 service mysql stop
 sleep 2;
