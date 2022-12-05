53dcad320b11579aa947d90e3dee2d79fedc49bd
Taha Chtaibi <tchtaibi@e2r3p5.1337.ma>
2022-12-05 14:44:22 +0100

    without password root

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,4 +1,3 @@
-# cp -f /wp-config.php /var/www/html/wordpress
 echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 mkdir -p /var/www/html/wordpress/wp-content/uploads/2022/ /var/www/html/wordpress/wp-content/uploads/2022/ /var/www/html/wordpress/wp-content/uploads/2022/12/
 cp /logo.png /var/www/html/wordpress/wp-content/uploads/2022/12/
